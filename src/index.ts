7091cfe60a7e246fe64d2b02c8d3ceea03c3290c
aleynaalangill <aleyna@firstbatch.xyz>
2023-08-08 14:31:03 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import {Request,Response} from "express";
+import serverless from "serverless-http";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req:Request, res:Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(5006, () => {
+    console.log('Server is listening on port 5006');
+});
+
+module.exports.handler = serverless(app);
